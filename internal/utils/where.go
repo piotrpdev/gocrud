2a24a4f460d69f3a8d9704f39ee8eb5640ff473e
KoLiBer <ckoliber@gmail.com>
2025-03-19 16:02:55 +0330

    feat: add where schema

--- internal/utils/where.go
+++ internal/utils/where.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/danielgtaylor/huma/v2"
 )
@@ -11,74 +12,76 @@ type Where[Model any] map[string]any
 // Schema returns a schema representing this value on the wire.
 // It returns the schema of the contained type.
 func (where *Where[Model]) Schema(r huma.Registry) *huma.Schema {
-	// schema := r.Schema(reflect.TypeOf(where.Value), true, "")
-	// Build schema for comparison operators
-	// comparisonSchema := &huma.Schema{
-	// 	Type: huma.TypeObject,
-	// 	Properties: map[string]*huma.Schema{
-	// 		"eq":  {Type: huma.TypeString},
-	// 		"neq": {Type: huma.TypeString},
-	// 		"gt":  {Type: huma.TypeNumber},
-	// 		"gte": {Type: huma.TypeNumber},
-	// 		"lt":  {Type: huma.TypeNumber},
-	// 		"lte": {Type: huma.TypeNumber},
-	// 		"in": {
-	// 			Type: huma.TypeArray,
-	// 			Items: &huma.Schema{
-	// 				Type: huma.TypeString,
-	// 			},
-	// 		},
-	// 	},
-	// }
-
+	ref := "#/components/schemas/Where"
 	schema := &huma.Schema{
+		Ref: ref,
 		OneOf: []*huma.Schema{
+			{
+				Type:                 huma.TypeObject,
+				Properties:           map[string]*huma.Schema{},
+				AdditionalProperties: false,
+			},
 			{
 				Type: huma.TypeObject,
 				Properties: map[string]*huma.Schema{
-					"foo": {Type: huma.TypeString},
+					"_not": {
+						Ref: ref,
+					},
 				},
 			},
 			{
-				Type: huma.TypeArray,
-				Items: &huma.Schema{
-					Type: huma.TypeObject,
-					Properties: map[string]*huma.Schema{
-						"foo": {Type: huma.TypeString},
+				Type: huma.TypeObject,
+				Properties: map[string]*huma.Schema{
+					"_and": {
+						Type: huma.TypeArray,
+						Items: &huma.Schema{
+							Ref: ref,
+						},
+					},
+				},
+			},
+			{
+				Type: huma.TypeObject,
+				Properties: map[string]*huma.Schema{
+					"_or": {
+						Type: huma.TypeArray,
+						Items: &huma.Schema{
+							Ref: ref,
+						},
 					},
 				},
 			},
 		},
 	}
 
-	// schema.OneOf = append(schema.OneOf,
-	// 	&huma.Schema{
-
-	// 	&huma.Schema{
-	// 		Type: huma.TypeObject,
-	// 		Properties: map[string]*huma.Schema{
-	// 			"_or": {
-	// 				Type:  huma.TypeArray,
-	// 				Items: schema,
-	// 			},
-	// 		},
-	// 	},
-	// 	&huma.Schema{
-	// 		Type: huma.TypeObject,
-	// 		Properties: map[string]*huma.Schema{
-	// 			"_and": {
-	// 				Type:  huma.TypeArray,
-	// 				Items: schema,
-	// 			},
-	// 		},
-	// 	},
-	// 	&huma.Schema{
-	// 		Type: huma.TypeObject,
-	// 		Properties: map[string]*huma.Schema{
-	// 			"_not": schema,
-	// 		},
-	// 	},
-	// )
+	modelType := reflect.TypeFor[Model]()
+	for i := range modelType.NumField() {
+		field := modelType.Field(i)
+		schema.OneOf[0].Properties[field.Tag.Get("json")] = &huma.Schema{
+			OneOf: []*huma.Schema{
+				{
+					Type:                 huma.TypeObject,
+					Properties:           map[string]*huma.Schema{"_lt": {Type: huma.TypeString}},
+					AdditionalProperties: false,
+				},
+				{
+					Type:                 huma.TypeObject,
+					Properties:           map[string]*huma.Schema{"_gt": {Type: huma.TypeString}},
+					AdditionalProperties: false,
+				},
+				{
+					Type:                 huma.TypeObject,
+					Properties:           map[string]*huma.Schema{"_lte": {Type: huma.TypeString}},
+					AdditionalProperties: false,
+				},
+				{
+					Type:                 huma.TypeObject,
+					Properties:           map[string]*huma.Schema{"_gte": {Type: huma.TypeString}},
+					AdditionalProperties: false,
+				},
+			},
+		}
+	}
 
 	return schema
 }
