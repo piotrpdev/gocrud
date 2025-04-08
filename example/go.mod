d62a2ce7d43856de267762cced368f0046c6fe71
KoLiBer <ckoliber@gmail.com>
2025-04-08 19:45:40 +0330

    docs: add all database examples

--- example/go.mod
+++ example/go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/ckoliber/gocrud v0.0.0-20250408132606-3056c6581ddb
 	github.com/danielgtaylor/huma/v2 v2.32.0
 	github.com/go-sql-driver/mysql v1.9.2
+	github.com/lib/pq v1.10.9
+	github.com/mattn/go-sqlite3 v1.14.27
+	github.com/microsoft/go-mssqldb v1.8.0
 )
 
 require (
@@ -14,9 +17,14 @@ require (
 	github.com/danielgtaylor/shorthand/v2 v2.2.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 )
--- example/postgres/main.go
+++ example/postgres/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
 	"time"
@@ -10,7 +11,10 @@ import (
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
+	"github.com/danielgtaylor/huma/v2/autopatch"
 	"github.com/danielgtaylor/huma/v2/humacli"
+
+	_ "github.com/lib/pq"
 )
 
 type Options struct {
@@ -18,18 +22,18 @@ type Options struct {
 }
 
 type User struct {
-	_    struct{} `json:"user"`
-	ID   string   `json:"id"`
-	Name string   `json:"name" maxLength:"30" example:"David" doc:"User name"`
-	Age  int      `json:"age" minimum:"1" maximum:"120" example:"25" doc:"User age from 1 to 120"`
+	_    struct{} `db:"users" json:"-"`
+	ID   *int     `db:"id" json:"id" required:"false"`
+	Name *string  `db:"name" json:"name" required:"false" maxLength:"30" example:"David" doc:"User name"`
+	Age  *int     `db:"age" json:"age" required:"false" minimum:"1" maximum:"120" example:"25" doc:"User age from 1 to 120"`
 }
 
 type Document struct {
-	_       struct{} `json:"document"`
-	ID      string   `json:"id"`
-	Title   string   `json:"title" maxLength:"50" doc:"Document title"`
-	Content string   `json:"content" maxLength:"500" doc:"Document content"`
-	UserID  string   `json:"userId" doc:"Document userId"`
+	_       struct{} `db:"documents" json:"-"`
+	ID      *int     `db:"id" json:"id" required:"false"`
+	Title   string   `db:"title" json:"title" maxLength:"50" doc:"Document title"`
+	Content string   `db:"content" json:"content" maxLength:"500" doc:"Document content"`
+	UserID  int      `db:"userId" json:"userId" doc:"Document userId"`
 }
 
 func main() {
@@ -41,8 +45,14 @@ func main() {
 
 		api.UseMiddleware()
 
-		gocrud.Register[User](api)
-		gocrud.Register[Document](api)
+		db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=postgres password=password dbname=postgres sslmode=disable")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		gocrud.Register(api, gocrud.NewSQLRepository[User](db), &gocrud.Config[User]{})
+		gocrud.Register(api, gocrud.NewSQLRepository[Document](db), &gocrud.Config[Document]{})
+		autopatch.AutoPatch(api)
 
 		// Create the HTTP server.
 		server := http.Server{
